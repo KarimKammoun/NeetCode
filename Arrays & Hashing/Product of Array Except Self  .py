05882986ebe18296051dc55f76588c3c70e72850
Karim Kammoun <128716985+KarimKammoun@users.noreply.github.com>
2023-08-12 15:21:26 +0100

    Neet_Code

--- Arrays & Hashing/Product of Array Except Self  .py
+++ Arrays & Hashing/Product of Array Except Self  .py
@@ -11,8 +11,7 @@ class Solution(object):
         for i in range(2,len_nums+2):
             liste1[-i]=liste1[-i+1]*nums[-i+1]
         for i in range(1,len_nums+1):
-            liste2[i]=liste2[i-1]*nums[i-1] 
-        for i in range(len_nums):
-            solution[i]=liste1[i+1]*liste2[i]
+            liste2[i]=liste2[i-1]*nums[i-1]
+            solution[i-1]=liste1[i]*liste2[i-1]
         return solution
         
