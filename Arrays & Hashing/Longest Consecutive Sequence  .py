406ba968cd2ea1a132145b85505ba6f7f5f0a8e1
Karim Kammoun <128716985+KarimKammoun@users.noreply.github.com>
2023-08-21 11:36:42 +0100

    NeetCode

---
+++ Arrays & Hashing/Longest Consecutive Sequence  .py
@@ -0,0 +1,21 @@
+lass Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        a=0
+        b=1
+        if len(nums)>0:
+            a=1
+        for i in range(0,len(nums)-1):
+            if nums[i]==nums[i+1]:
+                continue
+            if nums[i]==nums[i+1]-1 :
+                b=b+1
+                if b>a:
+                    a=b
+            else:
+                b=1
+        return a
