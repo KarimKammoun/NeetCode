967d33802fe877b1afe84baf9694bae1171c9697
karim kammoun <karimsfax06@gmail.com>
2024-08-23 23:17:01 +0100

    Neet_Code

--- stack/python/Generate_Parentheses.py
+++ stack/python/Generate_Parentheses.py
@@ -1,42 +1,18 @@
-class Solution(object):
-    def generateParenthesis(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
+class Solution:
+    def generateParenthesis(self, n) :
+        stack = []
         res = []
-
-        def search(ex, liste):
-            if len(ex) == 2 * n:
-                res.append(ex)
+        def backtrack(openN, closedN):
+            if openN == closedN == n:
+                res.append("".join(stack))
                 return
-            for i in range(2):
-                if liste[i][1] == 0:
-                    continue
-                liste[i][1] -= 1
-                search(ex + liste[i][0], liste)
-                liste[i][1] += 1
-
-        search("", [["(", n], [")", n]])
-        
-        def isValid(s):
-            stack = []
-            dic = {")": "(", "]": "[", "}": "{"}
-            for i in range(len(s)):
-                if s[i] in dic:
-                    if len(stack) > 0 and stack[-1] == dic[s[i]]:
-                        stack.pop()
-                    else:
-                        return False
-                else:
-                    stack.append(s[i])
-            return len(stack) == 0
-
-        i = 0
-        while i < len(res):
-            if not isValid(res[i]):
-                del res[i]
-                continue
-            i += 1
-
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop()
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+        backtrack(0, 0)
         return res
