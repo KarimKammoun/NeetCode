3e5a8d54eeb74a9135fc1e25ba8e76521c6f3962
karim kammoun <karimsfax06@gmail.com>
2024-08-26 23:43:25 +0100

    Leet_Code

--- stack/python/Daily_Temperatures.py
+++ stack/python/Daily_Temperatures.py
@@ -1,16 +1,10 @@
-temperatures = [30,38,30,36,35,40,28]
-
-res=[]
-dic={}
-for i in range(len(temperatures)):
-    if (temperatures[i] in dic)==False:
-        dic[temperatures[i]]=i
-    
-    if i>0:
-        for j in range(temperatures[i]):
-            if j in dic:
-                res.append(i-dic[i])
-
-
-print(dic)
-print(res)
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+        stack = []
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackInd = stack.pop()
+                res[stackInd] = i - stackInd
+            stack.append((t, i))
+        return res
