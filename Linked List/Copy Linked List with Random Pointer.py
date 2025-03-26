48f6dc84f3da28842ab5aa9073d2a4cf94570cb1
karim kammoun <karimsfax06@gmail.com>
2025-03-26 16:19:03 +0100

    NEET_CODE

--- Linked List/Copy Linked List with Random Pointer.py
+++ Linked List/Copy Linked List with Random Pointer.py
@@ -1,22 +1,38 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return None
 
 
-head = [[3,None],[7,3],[4,0],[5,1]]
+        i = head
+        while i:
+            new_node = Node(i.val, i.next, None)
+            i.next = new_node
+            i = new_node.next 
 
-liste=[]
+        i = head
+        while i:
+            if i.random:
+                i.next.random = i.random.next
+            i = i.next.next  
 
 
-i=head
+        i = head
+        new_head = head.next
+        copy = new_head
+        while i:
+            i.next = i.next.next
+            copy.next = copy.next.next if copy.next else None
+            i = i.next
+            copy = copy.next
 
-pos=0
-while(i!=None):
-    inter=[]
-    inter.append(i.val)
-    j=i
-    indice=pos
-    while(j!=i.random):
-        j=j.suivant
-        indice+=1
-    inter.append(indice)
-    liste.append(inter)
-    pos+=1
-print(liste)
+        return new_head
